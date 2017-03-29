cd22822475b4de71873d0cee000147b208e76d91
zhanghongbo <zhanghb@mmwipo.com>
2017-03-29 09:27:58 +0800

    fix all

--- TRZXBusinessPlan/TRZXBusinessPlan/Other/Target_BusinessPlan.h
+++ TRZXBusinessPlan/TRZXBusinessPlan/Other/Target_BusinessPlan.h
@@ -13,10 +13,31 @@
 
 
 
+/**
+ ..
+
+ @param params ..
+ @return ..
+ */
 - (UIViewController *)Action_TRZXBusinessPlan_TRZXFinalBusinessViewController:(NSDictionary *)params;
 
+
+
+/**
+ 我得商业计划书
+
+ @param params ..
+ @return ..
+ */
 - (UIViewController *)Action_TRZXBusinessPlan_KipoMyBusinessPlanViewController:(NSDictionary *)params;
 
+
+/**
+ 生成商业计划书
+
+ @param params ..
+ @return ..
+ */
 - (UIViewController *)Action_TRZXComplaint_TRZXBusinessPlan_TRZXHaveBusinessPlanController:(NSDictionary *)params;
 
 
